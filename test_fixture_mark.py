78b87db1a88697876ae23d69b937599d2a13246d
Aleksandr_K <amwnn@mail.ru>
2025-07-11 22:00:24 +0300

    11.07.2025

---
+++ test_fixture_mark.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1:
+    # pytest -s -v -m smoke test_fixture8.py
+    # pytest -s -v -m "smoke or regression" test_fixture8.py
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    # pytest -s -v -m "not smoke" test_fixture8.py
+    # pytest -s -v -m "regression and win10" test_fixture81.py
+    @pytest.mark.regression
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
